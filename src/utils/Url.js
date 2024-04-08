5d67dedd711f43cb3ee0286fade196b259e31170
pawan <ps9502748@gmail.com>
2024-04-08 17:17:33 +0530

    url

--- src/utils/Url.js
+++ src/utils/Url.js
@@ -2,9 +2,9 @@ function client_url() {
   return 'http://localhost:8000';
 }
 function server_url() {
-  return 'http://localhost:8000';
-  // return 'https://new-live-gurez.onrender.com' 
-  // return 'https://new-live-git-main-gurez001s-projects.vercel.app' 
+  // return 'http://localhost:8000';
+  // return 'https://gurez-live.onrender.com' 
+  return 'https://new-live-git-main-gurez001s-projects.vercel.app' 
 }
 
 export {client_url,server_url};
